7cb37bd7fd990af43dd8b8ff4146600311af5b8f
ShitpostBot <brett.orange.memes@gmail.com>
2016-01-02 18:38:24 +1100

    fixed blank template showing after all was reviewed

--- spb/template-review.php
+++ spb/template-review.php
@@ -1,12 +1,14 @@
 <?php
 include('php/autoload.php');
-$template = $db->getTemplates("SELECT t.*
+$templates = $db->getTemplates("SELECT t.*
 				   FROM Templates as t 
 				   WHERE t.reviewState = 'p'
 				   ORDER BY random()
-				   LIMIT 1")[0];
-
+				   LIMIT 1");
+if(count($templates) > 0){
+	$template = $templates[0];
+}
 $count = $db->scalar("SELECT count(*) FROM Templates WHERE reviewState = 'p'", array(), array());
-echo $twig->render('template-review.html', $row === false ? array('msg' => 'No templates found.') : array('template' => $template, 'count' => $count));
+echo $twig->render('template-review.html', count($templates) == 0 ? array('msg' => 'No templates found.') : array('template' => $template, 'count' => $count));
 $db->close();
 ?>
