69b28e9a839d32e6648bc5ad1cc91b88b5f3f6d3
ShitpostBot <brett.orange.memes@gmail.com>
2016-01-09 00:51:12 +1100

    shit

--- spb/profile.php
+++ spb/profile.php
@@ -6,8 +6,8 @@ $users = $db->getUsers('SELECT * FROM Users WHERE userId = ? OR username = ?', a
 if(count($users) > 0){
 	$user = $users[0];
 	$userExists = true;
-	$templates = $db->getTemplates("SELECT * FROM Templates WHERE userId = ? AND reviewState = 'a' ORDER BY timeAdded", array($userId), array(SQLITE3_TEXT));
-	$sourceImages = $db->getSourceImages("SELECT * FROM SourceImages WHERE userId = ? AND reviewState = 'a' ORDER BY timeAdded", array($userId), array(SQLITE3_TEXT));
+	$templates = $db->getTemplates("SELECT * FROM Templates WHERE userId = ? OR username = ? AND reviewState = 'a' ORDER BY timeAdded", array($userId, $userId), array(SQLITE3_TEXT, SQLITE3_TEXT));
+	$sourceImages = $db->getSourceImages("SELECT * FROM SourceImages WHERE userId = ? OR username = ? AND reviewState = 'a' ORDER BY timeAdded", array($userId, $userId), array(SQLITE3_TEXT, SQLITE3_TEXT));
 	$admin = $user->getAdmin();
 }
 echo $twig->render('profile.html', array('userExists' => $userExists, 'user' => $user, 'admin' => $admin, 'templates' => $templates, 'sourceImages' => $sourceImages));
